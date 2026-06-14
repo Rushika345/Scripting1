7ad47de6af4f1508aa7c9123b00bc7540e787e60
Rushika@345 <rushikahatwar345@gmail.com>
2026-06-14 21:21:49 +0530

    One

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
-echo "We are learning now Jenkin 
-echo "We are learning each topic ."
+echo "We are learning each topic."
 
